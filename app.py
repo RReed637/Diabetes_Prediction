e7478eac380594ed9f599d134cc878850e4375a9
RReed637 <158175741+RReed637@users.noreply.github.com>
2024-02-08 00:35:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,45 @@
 import streamlit as st
 import pandas as pd
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
-
 st.title("""Screening Application for Diabetes""")
-data = CustomData(
-    gender = st.selectbox('Select Sex', ("Male", "Female")),
-    age = st.slider('Your Age:', min_value = 1, max_value = 100, step=5 ),
-    hypertension = st.selectbox('Do you have hypertension:', ("Yes", "No")),
-    bmi = st.slider('Your BMI:', min_value= 1, max_value = 100, step=10),
-    heart_disease = st.selectbox('Have you contracted Heart Disease?:',    ("Yes", "No")),
-    HbA1c_level = st.slider('What is your HbA1c Level (Average Blood Sugar Levels for the last two to three months):', 1,10, 1),
-    blood_glucose_level = st.slider('What is your Blood Glucose Level (Blood Sugar level):', 50, 300, 5)
-)
 def prediction():
+    data=CustomData(
+        gender = st.sidebar.selectbox('Select Sex', ("Male", "Female")),
+        age = st.sidebar.slider('Your Age:', min_value = 1, max_value = 100, step=5 ),
+        hypertension = st.sidebar.selectbox('Do you have hypertension:', ("Yes", "No")),
+        bmi = st.sidebar.slider('Your BMI:', min_value= 1, max_value = 100, step=10),
+        heart_disease = st.sidebar.selectbox('Have you contracted Heart Disease?:',    ("Yes", "No")),
+        HbA1c_level = st.sidebar.slider('What is your HbA1c Level (Average Blood Sugar Levels for the last two to three months):', 1,10, 1),
+        blood_glucose_level = st.sidebar.slider('What is your Blood Glucose Level (Blood Sugar level):', (50, 300, 5))
+    )
     pred_df=data.get_data_as_frame()    
     predict_pipeline=PredictPipeline()
     results=predict_pipeline.predict(pred_df)
-
     
-    return prediction
-
-results = prediction
-
-
-
-
-
-    
-
-# Web Application
-def app():
-    st.subheader('Prediction: ')
     print(results)
-app()
+    if(results[0]==0):
+        return 'not diabetic'
+    else:
+        return 'diabetic'
+
+def main():
+    st.title('Diabetes Prediction Application')
+    gender = st.sidebar.selectbox('Select Sex', ("Male", "Female")),
+    age = st.sidebar.slider('Your Age:', min_value = 1, max_value = 100, step=5 ),
+    hypertension = st.sidebar.selectbox('Do you have hypertension:', ("Yes", "No")),
+    bmi = st.sidebar.slider('Your BMI:', min_value= 1, max_value = 100, step=10),
+    heart_disease = st.sidebar.selectbox('Have you contracted Heart Disease?:',    ("Yes", "No")),
+    HbA1c_level = st.sidebar.slider('What is your HbA1c Level (Average Blood Sugar Levels for the last two to three months):', 1,10, 1),
+    blood_glucose_level = st.sidebar.slider('What is your Blood Glucose Level (Blood Sugar level):', (50, 300, 5))
+
+    Diagnosis=''
+
+    if st.button('Diabetes Test Result'):
+        Diagnosis = prediction([gender,age,hypertension,bmi,heart_disease,HbA1c_level,blood_glucose_level])
+    st.success(Diagnosis)
+
+if __name__=='__main__':
+    main()
+
+
+    
