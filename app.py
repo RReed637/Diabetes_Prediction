79fa4aa484767e27c1e96486eb5760db1545af77
RReed637 <158175741+RReed637@users.noreply.github.com>
2024-02-07 22:54:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,15 @@ def prediction():
     
     return prediction
 
+    gender = st.sidebar.selectbox('Select Sex', ("Male", "Female")),
+    age = st.sidebar.slider('Your Age:', min_value = 1, max_value = 100, step=5 ),
+    hypertension = st.sidebar.selectbox('Do you have hypertension:', ("Yes", "No")),
+    bmi = st.sidebar.slider('Your BMI:', min_value= 1, max_value = 100, step=10),
+    heart_disease = st.sidebar.selectbox('Have you contracted Heart Disease?:',    ("Yes", "No")),
+    HbA1c_level = st.sidebar.slider('What is your HbA1c Level (Average Blood Sugar Levels for the last two to three months):', 1,10, 1),
+    blood_glucose_level = st.sidebar.slider('What is your Blood Glucose Level (Blood Sugar level):', (50, 300, 5))
+    
+
 results = prediction
 
 
